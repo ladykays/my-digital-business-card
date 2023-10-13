9214436cce553721015c9838b9ef41cb8d30a577
Kerrion Siokwu <ladykerrion@sky.com>
2023-10-14 00:52:24 +0100

    add component and create structure for the project

---
+++ components/Info.jsx
@@ -0,0 +1,10 @@
+export default function Info() {
+  return (
+    <header>
+      <img src="https://dummyimage.com/317/ffffff/000000" alt="Ndidi" className="header-photo" />
+      <h1 className="header--name">Ndidiamaka Siokwu</h1>
+      <p className="header--role">Frontend Web Developer</p>
+      <p className="header--website">kre8tive.co.uk</p>
+    </header>
+  )
+}
